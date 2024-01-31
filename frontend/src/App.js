36feff8655f203ae9d2bdd35df1b391e76812fe0
GauravMarathe <gauravmarathe6468@gmail.com>
2024-01-31 14:19:48 +0530

    auth context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,20 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Signin from "./login/Signin/Signin";
-import Signup from "./login/Signup/Signup";
-import Home from "./Home";  // Import the Home component
+// App.js
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Signin from './login/Signin/Signin';
+import Signup from './login/Signup/Signup';
+import Home from './Home'; // Import the Home component
+import { AuthProvider } from './AuthContext'; // Import AuthProvider
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signin" element={<Signin />} />
-      <Route path="/signup" element={<Signup />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </AuthProvider>
   );
 };
 
--- frontend/src/componentshome/navbar/Navbar.jsx
+++ frontend/src/componentshome/navbar/Navbar.jsx
@@ -1,10 +1,13 @@
+// Navbar.js
 import React, { useState } from 'react';
 import './navbar.css';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { useAuth } from '../../AuthContext';
 
 const Navbar = () => {
+  const { user, signOut } = useAuth();
   const [showMenu, setShowMenu] = useState(false);
 
   const toggleMenu = () => {
@@ -15,20 +18,39 @@ const Navbar = () => {
     <div>
       <nav className={`navbar ${showMenu ? 'active' : ''}`}>
         <div className="navbar-brand">
-          <a className="navbar-logo" href="/"></a>
+          <Link className="navbar-logo" to="/"></Link>
         </div>
         <button className="menu-button" onClick={toggleMenu}>
           <div className={`menu-icon ${showMenu ? 'active' : ''}`}>
-            <span></span> 
+            <span></span>
             <span></span>
             <span></span>
           </div>
         </button>
         <div className="navbar-menu">
           <ul className={`navbar-items ${showMenu ? 'active' : ''}`}>
-            <li><Link to={'/'}>Home</Link></li>
-            <li><Link to={'/'}>Dashboard</Link></li>
-            <li><Link to={'/signup'}><FontAwesomeIcon icon={faUser} /></Link></li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/">Dashboard</Link>
+            </li>
+            {user ? (
+              <>
+                <li>
+                  {/* Link to home page */}
+                  <Link to="/">
+                    <FontAwesomeIcon icon={faUser} />
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <li>
+                <Link to="/signup">
+                  <FontAwesomeIcon icon={faUser} />
+                </Link>
+              </li>
+            )}
           </ul>
         </div>
       </nav>
